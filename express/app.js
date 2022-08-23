cdf3dccc32b2ab961a65b1bea868432e39fa2d2c
陈元皓 <chenyuanhao@heilansc.com>
2022-08-23 13:22:46 +0800

    添加热更新

--- express/app.js
+++ express/app.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3333
 
 app.get('/test', (req, res) => {
-  console.log(req);
+  console.log(req.query);
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+module.exports = app;
